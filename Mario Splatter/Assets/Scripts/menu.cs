c23e203bc0cc2aadedd6dcfaa935623ba7c1dde9
Malware221199 <samuele.starita@gmail.com>
2023-01-05 10:27:22 +0100

    s

---
+++ Mario Splatter/Assets/Scripts/menu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+   public void newgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void loadgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void loadlevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void options()
+    {
+
+    }
+    public void exit()
+    {
+
+    }
+}
