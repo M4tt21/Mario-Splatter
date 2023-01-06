13d846b98f6af046e6e3e3bd3b4defe77f28e892
Malware221199 <samuele.starita@gmail.com>
2023-01-06 17:06:44 +0100

    s

--- Mario Splatter/Assets/Scripts/Gun.cs
+++ Mario Splatter/Assets/Scripts/Gun.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    private int playerLayerMask;
+    private GameObject gun;
+    private GameObject camera;
+    private GameObject rifle;
+    private GameObject shotgun;
+    private GameObject pistol;
     // Start is called before the first frame update
     void Start()
     {
+        playerLayerMask = 2 << LayerMask.NameToLayer("Ignore Raycast");
+        playerLayerMask = ~playerLayerMask;
 
+        gun = GameObject.FindGameObjectWithTag("Guns");
+        camera= GameObject.FindGameObjectWithTag("MainCamera");
+        rifle=GameObject.FindGameObjectWithTag("rifle");
+        shotgun=GameObject.FindGameObjectWithTag("shotgun");
+        pistol=GameObject.FindGameObjectWithTag("pistol");
     }
 
     // Update is called once per frame
@@ -18,24 +31,27 @@ public class Gun : MonoBehaviour
     private void FixedUpdate()
     {
 
+        
+        Debug.DrawRay(camera.transform.position, camera.transform.forward * 1000, Color.red); // traiettoria proiettile visibile
         RaycastHit hitPoint;
 
-        if (Input.GetMouseButton(0))//tasto sinistro mouse
+        if (Input.GetButton("Fire1"))//tasto sinistro mouse
         {
-            bool raycastRes = Physics.Raycast(transform.position, transform.forward, out (hitPoint), Mathf.Infinity);
-            if (raycastRes)
+            bool raycastResCamera = Physics.Raycast(camera.transform.position, camera.transform.forward, out (hitPoint), Mathf.Infinity);
+            bool isNotValid = Physics.Linecast(rifle.transform.position,hitPoint.transform.position,playerLayerMask);
+            Debug.DrawLine(rifle.transform.GetChild(0).transform.position, hitPoint.transform.position) ;
+            Debug.Log("Collision: " + isNotValid); //traiettoria proiettile debug
+            if (raycastResCamera && isNotValid)
             {
+
                 //Debug.Log("Collision at: " + hitPoint.distance); //traiettoria proiettile debug
-                Debug.DrawRay(transform.position, transform.forward * 1000, Color.red); // traiettoria proiettile visibile
-                if (!hitPoint.transform.CompareTag("floor"))//se non spara al pavimento allora...
-                {
-                    Destroy(hitPoint.transform.gameObject);//distruggi l'oggetto colpito 
-                }
+               
+               
                 //Alternativa per distruggere i cloni
-                /*if (hitPoint.transform.CompareTag("Clone"))
+                if (hitPoint.transform.CompareTag("Blooper"))
                 {
                     Destroy(hitPoint.transform.gameObject);
-                }*/
+                }
             }
         }
     }
