0c0bfd33736ccf436f1021cfc22d93c8602020e7
M4tt21 <senismattia1@gmail.com>
2023-05-25 13:24:03 +0200

    s

--- Mario Splatter/Assets/Scripts/PauseMenu.cs
+++ Mario Splatter/Assets/Scripts/PauseMenu.cs
@@ -7,62 +7,36 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     //public GameObject player;
-    public GameObject pauseMenu;
-    public static bool isPaused;
+    [SerializeField]
+    public CanvasScript cs;
+
     void Start()
     {
-        pauseMenu.SetActive(false);
+        gameObject.SetActive(false);
     }
     void Update()
     {
-        if (Input.GetButton("Cancel"))
-        {
-            Debug.Log("ciao");
-            if (isPaused)
-            {
-                ResumeGame();
-            }
-
-            else
-            {
-                PauseGame();
-            }
-        }
+       
     }
     public void Resume()
     {
-        PlayerPrefs.DeleteAll();
-        PlayerPrefs.SetInt("CurrentLevel", 3);
-        SceneManager.LoadScene(PlayerPrefs.GetInt("CurrentLevel"));
+        cs.ResumeGame();
     }
     public void BackToMenu()
     {
-        SceneManager.LoadScene(PlayerPrefs.GetInt("CurrentLevel"));
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(PlayerPrefs.GetInt("menu"));
+        
     }
     public void Options()
     {
-        PlayerPrefs.SetInt("CurrentLevel", 1);
-        SceneManager.LoadScene(PlayerPrefs.GetInt("CurrentLevel"));
+        
     }
     public void Exit()
     {
         Application.Quit();
 
     }
-    public void PauseGame()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-        isPaused = true;
-        Cursor.visible = true;
-    }
-    public void ResumeGame()
-    {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
-        isPaused = false;
-        Cursor.visible = false;
-
-    }
+    
 }
 
