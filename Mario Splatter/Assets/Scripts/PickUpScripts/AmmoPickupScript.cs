d49a89eca91417755b4befe010a4c19029664404
Malware221199 <samuele.starita@gmail.com>
2023-06-03 18:32:21 +0200

    s

--- Mario Splatter/Assets/Scripts/PickUpScripts/AmmoPickupScript.cs
+++ Mario Splatter/Assets/Scripts/PickUpScripts/AmmoPickupScript.cs
@@ -11,17 +11,6 @@ public class AmmoPickupScript : MonoBehaviour
     [Header("Amount of Ammo")]
     [SerializeField]
     public int amount;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
