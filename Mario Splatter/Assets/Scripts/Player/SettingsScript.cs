f2da50ce9a455ad0f310c1e87d840b959626dee1
Malware221199 <samuele.starita@gmail.com>
2023-06-11 19:00:36 +0200

    s

--- Mario Splatter/Assets/Scripts/Player/SettingsScript.cs
+++ Mario Splatter/Assets/Scripts/Player/SettingsScript.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class SettingsScript : MonoBehaviour
 {
+    [Header("Settings")]
+    public float sens;
+    public float volume;
     public string reloadKey = "r";
+    public string Jump = "space";
+    public string Fire3 = "left shift";
+    public string Fire1 = "mouse 0";
+    public string Ar = "1";
+    public string Pump = "2";
+    public string Pistol = "3";
+    
+    
     void Start()
     {
         
