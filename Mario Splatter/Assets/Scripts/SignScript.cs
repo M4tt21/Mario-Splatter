a5d3fa4e66c8189b145631f223917e166d8b73c1
BugDimitru <francesco.melis@live.it>
2023-06-19 17:54:52 +0200

    sus

--- Mario Splatter/Assets/Scripts/SignScript.cs
+++ Mario Splatter/Assets/Scripts/SignScript.cs
@@ -5,26 +5,34 @@ using UnityEngine;
 
 public class SignScript : MonoBehaviour
 {
-    void Start()
+    void Update()
     {
         updateKeybindsSign();
     }
     public void updateKeybindsSign()
     {
+        Debug.Log("Sono dentro");
         GameObject signs = GameObject.FindGameObjectWithTag("Signs");
+        Debug.Log(signs);
         foreach (TextMeshProUGUI txt in signs.transform.GetComponentsInChildren<TextMeshProUGUI>())
         {
             switch (txt.gameObject.name)
             {
-                case "JumpSign":
-                    txt.SetText("premi" + SettingsScript.instance.jumpKey + "per saltare");
+                case "JumpSignText":
+                    Debug.Log("Cartello salto");
+                    txt.SetText("premi" +SettingsScript.instance.jumpKey + "per saltare");
+                    Debug.Log("Cartello salto scritto");
                     break;
 
-                case "SwitchWeaponSign":
+                case "SwitchWeaponSignText":
+                    Debug.Log("Cartello cambio arma");
                     txt.SetText("premi" + SettingsScript.instance.PKey +  SettingsScript.instance.SGKey +  SettingsScript.instance.ARKey + "per scegliere l'arma da equipaggiare");
+                    Debug.Log("Cartello cambio arma scritto");
                     break;
-                case "SprintSign":
+                case "SprintSignText":
+                    Debug.Log("Cartello corsa");
                     txt.SetText("premi" + SettingsScript.instance.sprintKey + "per correre");
+                    Debug.Log("Cartello corsa scritto");
                     break;
 
             }
